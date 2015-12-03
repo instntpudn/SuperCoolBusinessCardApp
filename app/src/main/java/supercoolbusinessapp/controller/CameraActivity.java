8b12f53ea4d17d47a04334772fa69a955e217ef7
Anthony <atk2@hotmail.com>
2015-12-02 19:19:57 -0500

    Added my Camera Code - AnthonyK

--- app/src/main/java/supercoolbusinessapp/controller/CameraActivity.java
+++ app/src/main/java/supercoolbusinessapp/controller/CameraActivity.java
@@ -2,11 +2,23 @@ package supercoolbusinessapp.controller;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 
 import sjcnymobile.supercoolbusinesscardapp.R;
 
@@ -14,34 +26,119 @@ import sjcnymobile.supercoolbusinesscardapp.R;
  * Created by AnthonyK on 11/29/2015.
  */
 public class CameraActivity extends AppCompatActivity {
+    public URI photo_uri;
+    public final String APP_TAG = "SuperBusinessCard";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
+    public String photoFileName = "new_card_photo.jpg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        Button mybutton_camera = (Button) findViewById(R.id.button_camera);
-        mybutton_camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+        Button cmdTakePic = (Button) findViewById(R.id.cmdTakePic);
+        cmdTakePic.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 takepicture();
             }
         });
-
     }
 
     void takepicture() {
-        //take a pic 12/1
+
         Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivityForResult(intent, 0);
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+        //startActivityForResult(intent, 1001);
+
+    }
+
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            File mediaStorageDir = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView myimage = (ImageView) findViewById(R.id.image_camera);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
 
-            myimage.setImageBitmap(imageBitmap);
+            Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+
+            Bitmap takenImage = rotateBitmapOrientation(takenPhotoUri.getPath().toString());
+
+            // by this point we have the camera photo on disk
+            // Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+            // Load the taken image into a preview
+            ImageView myimage = (ImageView) findViewById(R.id.imageView);
+
+            TextView txtdesc = (TextView) findViewById(R.id.txtdescription);
+            txtdesc.setText(takenPhotoUri.getPath().toString());
+
+            myimage.setImageBitmap(takenImage);
+
+            Button cmdTakePic = (Button) findViewById(R.id.cmdTakePic);
+            cmdTakePic.setText("Retake Picture");
+
+            Button cmdPostPic = (Button) findViewById(R.id.cmdPostPic);
+            cmdPostPic.setVisibility(0);
         }
+    }
+
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        // Read EXIF Data
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        // Return result
+        return rotatedBitmap;
     }
 }
